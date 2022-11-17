d28788c9683ad59877286695007d58b620f8201e
soumya-99 <soumyadeepmondal1234@gmail.com>
2022-11-18 00:05:15 +0530

    Added react router and boilerplate ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hey</h1>
-    </div>
-  )
+	return (
+		<Container className="my-4">
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="/new" element={<h1>New</h1>} />
+				<Route path="/:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" element={<h1>Edit</h1>} />
+				</Route>
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</Container>
+	)
 }
 
 export default App
