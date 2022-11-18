b11140fac6379148fdee1d9e8c0a99c1c08229b3
soumya-99 <soumyadeepmondal1234@gmail.com>
2022-11-18 19:03:32 +0530

    onCreateNote function added

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,30 @@ import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import NewNote from "./components/NewNote"
 import { useLocalStorage } from "./hooks/useLocalStorage"
-import { RawNote, Tag } from "./models/model"
+import { NoteData, RawNote, Tag } from "./models/model"
+import { v4 as uuidV4 } from "uuid"
 
 function App() {
 	const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
 	const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
-
 	const notesWithTags = useMemo(() => {
 		return notes.map((note) => {
 			return {
 				...note,
-				tags: tags.filter(tag => note.tagIds.includes(tag.id))
+				tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
 			}
 		})
 	}, [notes, tags])
+
+	function onCreateNote({ tags, ...data }: NoteData) {
+		setNotes((prevNotes) => {
+			return [
+				...prevNotes,
+				{ ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+			]
+		})
+	}
 
 	return (
 		<Container className="my-4">
